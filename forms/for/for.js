21a9f902de9141fd83f1a7fab04a705bbb4d54a0
asakurasaki <90635229+asakurasaki@users.noreply.github.com>
2021-09-22 16:20:05 -0500

    For loop finished

---
+++ forms/for/for.js
@@ -0,0 +1,5 @@
+let names = ['Bob', 'Janet', 'Tom', 'Erin', 'Lucy','Alyssa', 'Dustin']
+
+for (I = 0; I < names.length; I++){
+  console.log(`${names[I]}`)
+  }
---
+++ forms/while/while.js
@@ -0,0 +1,8 @@
+
+let user = prompt(`Enter number of iterations you want`)
+let I = 1
+while (I <user){
+  console.log(`This is loop iteration ${I}`)
+  I = I + 1
+}
+console.log(`this is the final iteration ${I}`)
