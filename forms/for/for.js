7a2d6f7cf631501f463220c0cc4d9a2cb18b1d89
asakurasaki <90635229+asakurasaki@users.noreply.github.com>
2021-09-24 14:20:12 -0500

    For branch finished

--- forms/for/for.js
+++ forms/for/for.js
@@ -1,13 +1,6 @@
-<<<<<<< Updated upstream
-=======
 
->>>>>>> Stashed changes
 let names = ['Bob', 'Janet', 'Tom', 'Erin', 'Lucy','Alyssa', 'Dustin']
 
 for (I = 0; I < names.length; I++){
   console.log(`${names[I]}`)
-<<<<<<< Updated upstream
   }
-=======
-  }
->>>>>>> Stashed changes
--- forms/while/while.js
+++ forms/while/while.js
@@ -5,8 +5,4 @@ while (I <user){
   console.log(`This is loop iteration ${I}`)
   I = I + 1
 }
-<<<<<<< Updated upstream
 console.log(`this is the final iteration ${I}`)
-=======
-console.log(`this is the final iteration ${I}`)
->>>>>>> Stashed changes
